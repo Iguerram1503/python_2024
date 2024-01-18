3ca13d50a801b138e69580816804a2e345d1688c
Iguerram1503 <guerraibrahim@gmail.com>
2024-01-17 20:51:19 -0600

    tipos de datos, números, operaciones aritméticas y funciones aritmeticas en python

---
+++ 004.math_functions.py
@@ -0,0 +1,10 @@
+'''
+004.math_functions
+Las funciones matematicas son codigos que ya existen en un modulo y se pueden usar sin programarlas
+'''
+
+print(min(12, 1, 5, 19))
+print(max(33, 56, 1, 3, 0))
+print(abs(-3.123))
+print(pow(3, 3))
+print(round(3.14151927121212, 2))
