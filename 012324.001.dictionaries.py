c8b8eca797f9afb8747f606bd8c2607a2d7f00b6
Iguerram1503 <guerraibrahim@gmail.com>
2024-01-23 20:33:04 -0600

    Agregando ejemplos de diccionarios, tuplas y sets, además una breve descripción de cada uno de ellos

---
+++ 012324.001.dictionaries.py
@@ -0,0 +1,16 @@
+'''
+001.Diccionarios
+
+Permiten guardar muchos datos difeerentes
+'''
+
+car_dict = {
+
+    'key' : 'Value',
+    'marca': 'VW',
+    'anio' : '2024',
+    'fabricacion' : ['calle conocida', 'colonia desconocida']
+    
+}
+
+print(car_dict['fabricacion'][0])
