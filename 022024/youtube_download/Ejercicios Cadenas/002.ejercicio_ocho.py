69fc28d8fb852d0b88683a7f00490cb267cd528b
Iguerram1503 <guerraibrahim@gmail.com>
2024-02-22 19:05:07 -0600

    Solucionando un ejercicio mas

--- 022024/youtube_download/Ejercicios Cadenas/002.ejercicio_ocho.py
+++ 022024/youtube_download/Ejercicios Cadenas/002.ejercicio_ocho.py
@@ -3,5 +3,8 @@ Ejercicio 8
 Escribir un programa que pregunte por consola el precio de un producto en euros con dos decimales y muestre por pantalla el número de pesos y el número de centavos del precio introducido.
 '''
 
-precio = input('Introduce una cantidad de dinero')
-print(precio[:precio.find('.')], 'pesos y', precio[precio.find('.')+1:])
+precio = input('Introduce una cantidad de dinero: ')
+pesos = precio[0:precio.find('.')]
+centavos = precio[precio.find('.') + 1:len(precio)]
+
+print(f'Usted tiene {pesos} pesos y {centavos} centavos')
